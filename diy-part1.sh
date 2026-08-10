4e0d98c8b8acfd1237642b3f63df1884483b1dc4
jiangyuqing <jiangyuqing@hopemobi.com>
2026-08-10 16:34:11 +0800

    Add custom LuCI packages and iStore

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,20 @@
-echo 'src-git kixdns https://github.com/JohnsonRan/luci-app-kixdns.git' >>feeds.conf.default
-# echo 'src-git daed https://github.com/QiuSimons/luci-app-daed.git' >>feeds.conf.default
-echo 'src-git kdae https://github.com/QiuSimons/luci-app-dae.git' >>feeds.conf.default
-echo 'src-git ddnsgo  https://github.com/sirpdboy/luci-app-ddns-go.git' >>feeds.conf.default
-# echo 'src-git openwrthonk  https://github.com/breeze303/openwrt-honk.git' >>feeds.conf.default
+#!/bin/bash
+set -euo pipefail
+
+cat <<'EOF' >>feeds.conf.default
+src-git kixdns https://github.com/JohnsonRan/luci-app-kixdns.git
+src-git kdae https://github.com/QiuSimons/luci-app-dae.git;kix
+src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git
+src-git quickfile https://github.com/sbwml/luci-app-quickfile.git;main
+src-git lucky https://github.com/gdy666/luci-app-lucky.git;main
+src-git easytier https://github.com/EasyTier/luci-app-easytier.git;main
+src-git istore https://github.com/linkease/istore.git;main
+EOF
+
+mkdir -p package/custom
+git clone --depth 1 --branch master \
+  https://github.com/eamonxg/luci-theme-aurora.git \
+  package/custom/luci-theme-aurora
+git clone --depth 1 --branch master \
+  https://github.com/eamonxg/luci-app-aurora-config.git \
+  package/custom/luci-app-aurora-config
